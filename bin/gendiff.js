fbaeadc94c310773bd6f768b6ff6a911c5403a0d
TIMUR <otto@MacBook-Air-Timur.local>
2022-10-05 13:34:52 +0300

    Add new file bin/gendiff.js

---
+++ bin/gendiff.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+import { program } from "commander";
+
+
+program
+  .description(`Compares two configuration files and shows a difference.`);
+
+program
+  .version('0.0.1', '-V, --version', 'output the version number');
+
+program.parse();
+
