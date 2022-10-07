d5530a7fd2c7b307f4c721c5aa2df1a02c8eacc1
TIMUR <otto@MacBook-Air-Timur.local>
2022-10-07 20:27:41 +0300

    Fix bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,13 +1,12 @@
 #!/usr/bin/env node
-import { program } from "commander";
+import { program } from 'commander';
+import genDiff from '../src/index.js';
 
 program
-  .description(`Compares two configuration files and shows a difference.`)
+  .description('Compares two configuration files and shows a difference.')
   .version('0.0.1', '-V, --version', 'output the version number')
-  .arguments('<filepath1> <filepath2>')
-  .helpOption('-h, --help', 'output usage information')
   .option('-f, --format <type>', 'output format')
-
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => console.log(genDiff(filepath1, filepath2)));
 
 program.parse(process.argv);
-
