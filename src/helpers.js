6174c51e1a16db71883ade9d4af7cd9569ff1a07
TIMUR <otto@MacBook-Air-Timur.local>
2022-10-09 13:35:22 +0300

    Fix src/helpers.js

--- src/helpers.js
+++ src/helpers.js
@@ -1,6 +1,9 @@
-import path from 'node:path';
+import { readFileSync } from 'node:fs';
 import { cwd } from 'node:process';
+import path from 'node:path';
 
 const getPath = (file) => path.resolve(cwd(), '__fixtures__', file);
+
+export const readFilePath = (filepath) => readFileSync(getPath(filepath), 'utf-8');
 
 export default getPath;
