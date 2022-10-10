90123caa943191444165b5845e67febe5cd56740
TIMUR <otto@MacBook-Air-Timur.local>
2022-10-10 16:59:58 +0300

    Add new file src/parsers.js

---
+++ src/parsers.js
@@ -0,0 +1,10 @@
+import yaml from 'js-yaml';
+
+const parser = (data, fileFormat) => {
+  if (fileFormat === '.yml' || fileFormat === '.yaml') {
+    return yaml.load(data);
+  }
+  return JSON.parse(data);
+};
+
+export default parser;
