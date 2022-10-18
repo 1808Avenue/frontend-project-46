b18aa7388438ebe42909d637354efcb32deba777
TIMUR <otto@MacBook-Air-Timur.local>
2022-10-18 14:32:54 +0300

    Fix __tests__/index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,17 +1,12 @@
+import { readFileSync } from 'node:fs';
 import { expect, test } from '@jest/globals';
 import genDiff from '../src/index.js';
+import getGlobalPath from '../src/helpers.js';
 
-test('gendiff', () => {
-  const filePath1 = '../__fixtures__/file1.json';
-  const filePath2 = '../__fixtures__/file2.json';
-  const expectedValue = `{
- - follow: false
-   host: hexlet.io
- - proxy: 123.234.53.22
- - timeout: 50
- + timeout: 20
- + verbose: true
-}`;
+test('gendiff-plain', () => {
+  const filePath1 = getGlobalPath('file1.json');
+  const filePath2 = getGlobalPath('file2.json');
+  const expectedValue = String(readFileSync(getGlobalPath('result.txt')));
 
   expect(genDiff(filePath1, filePath2)).toEqual(expectedValue);
   expect(genDiff('file1.json', 'file2.json')).toEqual(expectedValue);
