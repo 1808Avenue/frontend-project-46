7b0d8db755d4689537613b491bca9e2e48e040d5
TIMUR <otto@MacBook-Air-Timur.local>
2022-10-18 14:34:48 +0300

    Fix src/index.js

--- src/index.js
+++ src/index.js
@@ -1,35 +1,14 @@
-import _ from 'lodash';
 import { readFileSync } from 'node:fs';
-import getFullPath, { defineFileFormat } from './helpers.js';
+import defineDiff from './defineDiff.js';
+import stylish from './formatters/stylish.js';
+import getGlobalPath, { defineFileFormat } from './helpers.js';
 import parser from './parsers.js';
 
 const genDiff = (filePath1, filePath2) => {
-  const file1 = parser(readFileSync(getFullPath(filePath1), 'utf-8'), defineFileFormat(filePath1));
-  const file2 = parser(readFileSync(getFullPath(filePath2), 'utf-8'), defineFileFormat(filePath2));
-  const file1Keys = Object.keys(file1);
-  const file2Keys = Object.keys(file2);
-  const allKeys = file1Keys.concat(file2Keys);
-  const uniqueKeys = allKeys.reduce((acc, key) => {
-    if (acc.includes(key)) {
-      return acc;
-    }
-    return [...acc, key];
-  }, []);
-  const sortedUniqueKeys = _.sortBy(uniqueKeys);
-  const result = sortedUniqueKeys.reduce((acc, key) => {
-    if (file1Keys.includes(key) && file2Keys.includes(key)) {
-      if (file1[key] === file2[key]) {
-        return `${acc}\n   ${key}: ${file1[key]}`;
-      } if (file1[key] !== file2[key]) {
-        return `${acc}\n - ${key}: ${file1[key]}\n + ${key}: ${file2[key]}`;
-      }
-    }
-    if (file1Keys.includes(key) && !file2Keys.includes(key)) {
-      return `${acc}\n - ${key}: ${file1[key]}`;
-    }
-    return `${acc}\n + ${key}: ${file2[key]}`;
-  }, '');
-  return `{${result}\n}`;
+  const file1 = parser(readFileSync(getGlobalPath(filePath1), 'utf-8'), defineFileFormat(filePath1));
+  const file2 = parser(readFileSync(getGlobalPath(filePath2), 'utf-8'), defineFileFormat(filePath2));
+  const diff = defineDiff(file1, file2);
+  return stylish(diff);
 };
 
 export default genDiff;
