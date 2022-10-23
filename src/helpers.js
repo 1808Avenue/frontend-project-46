ae6fbe082f06b25cc4175493feeee823da09d6df
TIMUR <otto@MacBook-Air-Timur.local>
2022-10-23 17:17:29 +0300

    Fix src/helpers.js

--- src/helpers.js
+++ src/helpers.js
@@ -1,9 +1,7 @@
 import { cwd } from 'node:process';
 import path from 'node:path';
 
-// Получает полный путь до файла
-const getGlobalPath = (file) => path.resolve(cwd(), '__fixtures__', file);
-export default getGlobalPath;
+const getFixturePath = (file) => path.resolve(cwd(), '__fixtures__', file);
+export default getFixturePath;
 
-// Определяет формат файла
 export const defineFileFormat = (filePath) => path.extname(filePath);
