775e0ba6d8b60d13c4726e3fb9df9486076bb611
TIMUR <otto@MacBook-Air-Timur.local>
2022-10-23 17:20:09 +0300

    Fix __tests__/parsers.test.js

--- __tests__/parsers.test.js
+++ __tests__/parsers.test.js
@@ -1,14 +1,14 @@
 import { expect, test } from '@jest/globals';
 import { readFileSync } from 'node:fs';
-import getGlobalPath from '../src/helpers.js';
+import getFixturePath from '../src/helpers.js';
 import parser from '../src/parsers.js';
 
 test('parser', () => {
-  const data1 = readFileSync(getGlobalPath('file1.json'));
-  const data2 = readFileSync(getGlobalPath('file1.yml'));
-  const expectedJson = parser(readFileSync(getGlobalPath('file1.json')));
-  const expectedYml = parser(readFileSync(getGlobalPath('file1.yml')));
+  const data1 = readFileSync(getFixturePath('file1.json'));
+  const data2 = readFileSync(getFixturePath('file1.yaml'));
+  const expectedJson = parser(readFileSync(getFixturePath('file1.json')));
+  const expectedYaml = parser(readFileSync(getFixturePath('file1.yaml')));
 
   expect(parser(data1, '.json')).toEqual(expectedJson);
-  expect(parser(data2, '.yml')).toEqual(expectedYml);
+  expect(parser(data2, '.yaml')).toEqual(expectedYaml);
 });
