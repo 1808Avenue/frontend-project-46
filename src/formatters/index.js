fc2a583783e490a260d5f06eaf670237a14bf6e3
TIMUR <otto@MacBook-Air-Timur.local>
2022-10-25 17:02:46 +0300

    Fix src/formatters/index.js

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,6 +1,13 @@
 import stylish from './stylish.js';
 import plain from './plain.js';
 
-const defineOutputFormat = (formatName) => ((formatName === 'plain') ? plain : stylish);
-
+const defineOutputFormat = (formatName) => {
+  if (formatName === 'plain') {
+    return plain;
+  }
+  if (formatName === 'json') {
+    return JSON.stringify;
+  }
+  return stylish;
+};
 export default defineOutputFormat;
