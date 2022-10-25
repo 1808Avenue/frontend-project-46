622a70dff1f6f562a52cd56f4434e0197114d71d
TIMUR <otto@MacBook-Air-Timur.local>
2022-10-25 20:21:25 +0300

    Fix __tests__/index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -6,23 +6,12 @@ import genDiff from '../src/index.js';
 const filePath1 = getFixturePath('file1.yaml');
 const filePath2 = getFixturePath('file2.yaml');
 
-test('gendiff-stylish', () => {
+test('gendiff', () => {
   const expectedStylishValue = String(readFileSync(getFixturePath('result.stylish.txt')));
-
-  expect(genDiff(filePath1, filePath2)).toEqual(expectedStylishValue);
-  expect(genDiff('file1.yaml', 'file2.yaml')).toEqual(expectedStylishValue);
-});
-
-test('gendiff-plain', () => {
   const expectedPlainValue = String(readFileSync(getFixturePath('result.plain.txt')));
-
-  expect(genDiff(filePath1, filePath2, 'plain')).toEqual(expectedPlainValue);
-  expect(genDiff('file1.yaml', 'file2.yaml', 'plain')).toEqual(expectedPlainValue);
-});
-
-test('gendiff-json', () => {
   const expectedJsonValue = String(readFileSync(getFixturePath('result.json.txt')));
 
+  expect(genDiff(filePath1, filePath2)).toEqual(expectedStylishValue);
+  expect(genDiff(filePath1, filePath2, 'plain')).toEqual(expectedPlainValue);
   expect(genDiff(filePath1, filePath2, 'json')).toEqual(expectedJsonValue);
-  expect(genDiff('file1.yaml', 'file2.yaml', 'json')).toEqual(expectedJsonValue);
 });
