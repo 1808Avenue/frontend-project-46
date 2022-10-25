9c23b0bfd8a2527b8f11c493182240c0ff4fca02
TIMUR <otto@MacBook-Air-Timur.local>
2022-10-25 14:19:00 +0300

    Fix __tests__/index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,12 +1,12 @@
 import { readFileSync } from 'node:fs';
 import { expect, test } from '@jest/globals';
-import genDiff from '../src/index.js';
 import getFixturePath from '../src/helpers.js';
+import genDiff from '../src/index.js';
 
 test('gendiff', () => {
   const filePath1 = getFixturePath('file1.yaml');
   const filePath2 = getFixturePath('file2.yaml');
-  const expectedValue = String(readFileSync(getFixturePath('result.txt')));
+  const expectedValue = String(readFileSync(getFixturePath('result.stylish.txt')));
 
   expect(genDiff(filePath1, filePath2)).toEqual(expectedValue);
   expect(genDiff('file1.yaml', 'file2.yaml')).toEqual(expectedValue);
