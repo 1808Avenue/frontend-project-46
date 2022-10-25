d94f126759aa1f3b5babf68078a090653781eb1c
TIMUR <otto@MacBook-Air-Timur.local>
2022-10-25 20:11:25 +0300

    Fix src/index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import { readFileSync } from 'node:fs';
 import defineDiff from './defineDiff.js';
-import stylish from './formatters/stylish.js';
+import defineOutputFormat from './formatters/index.js';
 import getFixturePath, { defineFileFormat } from './helpers.js';
 import parser from './parsers.js';
 
-const genDiff = (filePath1, filePath2, format = stylish) => {
+const genDiff = (filePath1, filePath2, formatName = 'stylish') => {
   const file1 = parser(readFileSync(getFixturePath(filePath1), 'utf-8'), defineFileFormat(filePath1));
   const file2 = parser(readFileSync(getFixturePath(filePath2), 'utf-8'), defineFileFormat(filePath2));
   const diff = defineDiff(file1, file2);
-  return format(diff);
+  const formatOutput = defineOutputFormat(formatName);
+  return formatOutput(diff);
 };
 
 export default genDiff;
