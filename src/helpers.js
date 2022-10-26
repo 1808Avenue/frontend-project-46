5b58bdbb59215d3f971d13a556fe7030153e2c1b
TIMUR <otto@MacBook-Air-Timur.local>
2022-10-26 20:16:58 +0300

    Fix src/helpers.js

--- src/helpers.js
+++ src/helpers.js
@@ -4,4 +4,4 @@ import path from 'node:path';
 const getFixturePath = (file) => path.resolve(cwd(), '__fixtures__', file);
 export default getFixturePath;
 
-export const defineFileFormat = (filePath) => path.extname(filePath);
+export const defineFileFormat = (filePath) => path.extname(filePath).slice(1);
