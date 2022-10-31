ef6417f8aa3fec7ba4558fcbe10ec2abe5e432d9
TIMUR <otto@MacBook-Air-Timur.local>
2022-10-31 10:04:09 +0300

    Fix src/parsers.js

--- src/parsers.js
+++ src/parsers.js
@@ -1,10 +1,19 @@
 import yaml from 'js-yaml';
 
 const parser = (data, format) => {
-  if (format === 'yml' || format === 'yaml') {
-    return yaml.load(data);
+  switch (format) {
+    case 'json':
+      return JSON.parse(data);
+
+    case 'yml':
+      return yaml.load(data);
+
+    case 'yaml':
+      return yaml.load(data);
+
+    default:
+      throw new Error(`Unknown format: '${format}'!`);
   }
-  return JSON.parse(data);
 };
 
 export default parser;
